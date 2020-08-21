d397dfcef34ee8cf7162974bbdddd36ba42cf22b
CodeRuiner404 <zzhu0034@student.monash.edu>
2020-08-21 15:32:13 +0800

    A NEW CHANGE

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a = "ABc";
+            int* p = (int*)malloc(sizeof(int) * 4);
+            int i;
+
+            // p[] contains some garbage value so 
+            // below loop does not make any sense. 
+            for (i = 0; i < 4; i++)
+                p[i] += 100;
+        }
+    }
+}
